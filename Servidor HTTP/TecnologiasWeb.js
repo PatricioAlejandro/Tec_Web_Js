df23bca506f999c9ffa07ff00c3be57862af12b1
PatricioAlejandro <patricio.alejandroch@hotmail.com>
2016-11-30 14:39:12 -0500

    parametros

--- Servidor HTTP/TecnologiasWeb.js
+++ Servidor HTTP/TecnologiasWeb.js
@@ -1,11 +1,39 @@
 var express = require('express');
 var app = express();
 var puerto = 5050;
+var usuarios = [{
+    id: 1,
+    nombre: 'Pepe',
+    cedula: '00012374128'
+}, {
+    id: 2,
+    nombre: 'Carlos',
+    cedula: '13412498127'
+}, {
+    id: 3,
+    nombre: 'Juan',
+    cedula: '12837912874'
+}];
 
 
 app.get('/TecnologiasWeb', function (req, res) {
     res.send('con Javascript');
 });
+
+app.get('/Usuario/:idUsuario', function (req, res) {
+    var idActual = req.params.idUsuario;
+    for (var i = 0; i < usuarios.length; i++) {
+        if (idActual == usuarios[i].id) {
+            res.json(usuarios[i]);
+        }
+    }
+    res.send('No existe usuario');
+
+});
+app.get('/Usuario', function (req, res) {
+            res.json(usuarios);
+});
+
 app.post('/TecnologiasWeb', function (req, res) {
 
 
