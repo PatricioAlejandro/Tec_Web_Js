69c80c503ce159d6ff8a0f9e49c0874a220fa708
PatricioAlejandro <patricio.alejandroch@hotmail.com>
2016-11-30 13:46:45 -0500

    puerto

--- Servidor HTTP/TecnologiasWeb.js
+++ Servidor HTTP/TecnologiasWeb.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
-
+var puerto = 5050;
 app.post('/TecnologiasWeb', function (req, res) {
   res.send('con Javascript');
 });
 
-app.listen(5050, function () {
-  console.log('Ejemplo de javascript');
+app.listen(puerto, function () {
+  console.log('Ejemplo de javascript esta en el puerto:'+puerto);
 });
