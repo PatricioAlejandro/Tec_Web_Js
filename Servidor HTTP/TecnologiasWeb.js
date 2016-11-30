cd766b491bec1b9ad9ee922ca061393e1126fb71
PatricioAlejandro <patricio.alejandroch@hotmail.com>
2016-11-30 13:47:19 -0500

    methods get y post

--- Servidor HTTP/TecnologiasWeb.js
+++ Servidor HTTP/TecnologiasWeb.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var puerto = 5050;
+
+
+app.get('/TecnologiasWeb', function (req, res) {
+  res.send('con Javascript');
+});
 app.post('/TecnologiasWeb', function (req, res) {
   res.send('con Javascript');
 });
