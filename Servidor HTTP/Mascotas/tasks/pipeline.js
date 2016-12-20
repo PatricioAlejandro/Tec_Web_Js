fe2bb0ad8d95bd150099a82fc1538fe8116cb546
a <a>
2016-12-20 14:14:04 -0500

    inyeccion app.js

--- Servidor HTTP/Mascotas/tasks/pipeline.js
+++ Servidor HTTP/Mascotas/tasks/pipeline.js
@@ -28,7 +28,7 @@ var jsFilesToInject = [
 
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
-
+  'app/app.js',
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
 
@@ -63,26 +63,24 @@ var tmpPath = '.tmp/public/';
 // Prefix relative paths to source files so they point to the proper locations
 // (i.e. where the other Grunt tasks spit them out, or in some cases, where
 // they reside in the first place)
-module.exports.cssFilesToInject = cssFilesToInject.map(function(cssPath) {
-  // If we're ignoring the file, make sure the ! is at the beginning of the path
-  if (cssPath[0] === '!') {
-    return require('path').join('!.tmp/public/', cssPath.substr(1));
-  }
-  return require('path').join('.tmp/public/', cssPath);
+module.exports.cssFilesToInject = cssFilesToInject.map(function (cssPath) {
+    // If we're ignoring the file, make sure the ! is at the beginning of the path
+    if (cssPath[0] === '!') {
+        return require('path').join('!.tmp/public/', cssPath.substr(1));
+    }
+    return require('path').join('.tmp/public/', cssPath);
 });
-module.exports.jsFilesToInject = jsFilesToInject.map(function(jsPath) {
-  // If we're ignoring the file, make sure the ! is at the beginning of the path
-  if (jsPath[0] === '!') {
-    return require('path').join('!.tmp/public/', jsPath.substr(1));
-  }
-  return require('path').join('.tmp/public/', jsPath);
+module.exports.jsFilesToInject = jsFilesToInject.map(function (jsPath) {
+    // If we're ignoring the file, make sure the ! is at the beginning of the path
+    if (jsPath[0] === '!') {
+        return require('path').join('!.tmp/public/', jsPath.substr(1));
+    }
+    return require('path').join('.tmp/public/', jsPath);
 });
-module.exports.templateFilesToInject = templateFilesToInject.map(function(tplPath) {
-  // If we're ignoring the file, make sure the ! is at the beginning of the path
-  if (tplPath[0] === '!') {
-    return require('path').join('!assets/', tplPath.substr(1));
-  }
-  return require('path').join('assets/',tplPath);
+module.exports.templateFilesToInject = templateFilesToInject.map(function (tplPath) {
+    // If we're ignoring the file, make sure the ! is at the beginning of the path
+    if (tplPath[0] === '!') {
+        return require('path').join('!assets/', tplPath.substr(1));
+    }
+    return require('path').join('assets/', tplPath);
 });
-
-
