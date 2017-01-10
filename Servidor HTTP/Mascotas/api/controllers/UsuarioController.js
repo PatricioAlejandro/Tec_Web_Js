aa4a245f57c197fa79fc41a256bbf44a8274bc8d
PatricioAlejandro <patricio.alejandroch@hotmail.com>
2017-01-10 13:47:24 -0500

    crear Usuario

--- Servidor HTTP/Mascotas/api/controllers/UsuarioController.js
+++ Servidor HTTP/Mascotas/api/controllers/UsuarioController.js
@@ -6,6 +6,19 @@
  */
 
 module.exports = {
-	
-};
 
+    //acceder al siguiente metodo /Usuario/crearUsuario
+    crearUsuario: function (req, res) {
+        Usuario.create({
+            name: 'Juan'
+        }).exec(function (err, juan) {
+            if (err) {
+                return res.serverError(err);
+            }
+
+            sails.log('Finn\'s id is:', finn.id);
+            return res.ok();
+        });
+    }
+
+};
