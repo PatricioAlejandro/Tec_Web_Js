be003d0d55075dac2876d17941361916703691c0
PatricioAlejandro <patricio.alejandroch@hotmail.com>
2017-01-11 13:19:24 -0500

    crear rutas controller

---
+++ Servidor HTTP/Mascotas/api/controllers/RutasController.js
@@ -0,0 +1,14 @@
+/**
+ * RutasController
+ *
+ * @description :: Server-side logic for managing Rutas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+    home: function(req,res){
+        //res.view(String: nombre vista, datos json)
+        return  res.view('vistas/home')
+    }
+};
+
