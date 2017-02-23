739baa17f6d9ccaa6da925a6faec702e567f4f61
PatricioAlejandro <patricio.alejandroch@hotmail.com>
2017-02-23 17:54:26 -0500

    dos vias

--- ProyectoAngular/AplicacionPrueba/src/app/app.component.ts
+++ ProyectoAngular/AplicacionPrueba/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  title:string = "Hola Amigos";
+  nombre:string="";
+  apellido:string="";
+  colorBackGround="background-color: red;";
+  constructor(){
+    this.nombre="Pato";
+    this.apellido="Chavez";
+    console.log("Inicio Constructor");
+  }
+  ngOnInit(){
+    this.nombre="Alejo";
+    this.apellido="Melo";
+    console.log("Inicio OnInit");
+  }
+  nombreCompleto():string{
+    return `${this.nombre} - ${this.apellido}`
+  }
+
+  hizoClick(){
+    console.log("Hizo CLick");
+
+  }
+  hizoFocus(){
+    console.log("Hizo focus");
+  }
+  cambiarC
 }
