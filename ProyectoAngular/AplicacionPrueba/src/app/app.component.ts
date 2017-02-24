f2c024b59150f9da76c99622acf4c9c74c2b3ba0
PatricioAlejandro <patricio.alejandroch@hotmail.com>
2017-02-24 10:32:04 -0500

    conectar front y back end

--- ProyectoAngular/AplicacionPrueba/src/app/app.component.ts
+++ ProyectoAngular/AplicacionPrueba/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Http} from "@angular/http";
+import {MasterURLService} from "./services/master-url.service";
 
 @Component({
   selector: 'app-root',
@@ -6,35 +8,52 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title:string = "Hola Amigos";
-  nombre:string="";
-  apellido:string="";
-  colorBackGround="background-color: red;";
-
+  title: string = "Hola Amigos";
+  nombre: string = "";
+  apellido: string = "";
+  colorH4 = "red";
+  tamanoH4 = "52px";
+  classes = "btn btn-block btn-success";
   nuevaTienda:any={};
 
-  constructor(){
-    this.nombre="Pato";
-    this.apellido="Chavez";
-    console.log("Inicio Constructor");
-  }
-  ngOnInit(){
-    this.nombre="Alejo";
-    this.apellido="Melo";
-    console.log("Inicio OnInit");
-  }
-  nombreCompleto():string{
-    return `${this.nombre} - ${this.apellido}`
+
+  constructor(private _http: Http, private _masterURL:MasterURLService) {
+    this.apellido = "Chavez";
+    this.nombre = "Patricio";
+    console.log("Inicio el constructor");
   }
 
-  hizoClick(){
-    console.log("Hizo CLick");
-
+  ngOnInit() {
+    this.apellido = "Sarzosa";
+    this.nombre = "Vicente";
+    console.log("On Init")
   }
-  hizoFocus(){
+
+  nombreCompleto(): string {
+    return `${this.nombre} ${this.apellido}`
+  }
+
+  hizoClick() {
+    console.log("Hizo Click");
+    console.log()
+  }
+
+  hizoFocus() {
     console.log("Hizo focus");
   }
+
+
   crearTienda(formulario){
     console.log(formulario);
+    this._http
+      .post(this._masterURL.url, {})
+      .subscribe(
+        res=>console.log('Respuesta: ',res),
+        err=>console.log('Error: ',err),
+        ()=>{
+          console.log("Se completo la accion")
+        }
+      );
   }
+
 }
---
+++ ProyectoAngular/AplicacionPrueba/src/app/services/master-url.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MasterURLService {
+  private _url:string;
+  constructor() {
+    this._url = "http://localhost:1337";
+    //this.url = "http://asdasdasd-asdasd.c9users.io";
+  }
+  get url():string{
+    return this._url;
+  }
+  set url(nuevoUrl:string){
+    this._url=nuevoUrl;
+  }
+}
